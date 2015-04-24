5ccdafc8399ba7cab51bfd2ede1b5d5f9d5db29e
superit23 <dcfriendsms@gmail.com>
2015-04-23 20:04:18 -0500

    Updates to Reflux.

--- UnitTest/UnitTestAll.cs
+++ UnitTest/UnitTestAll.cs
@@ -29,7 +29,9 @@ namespace UnitTest
             //Reflux.FormatTable(subTest, testConn);
 
            //Reflux.InsertIntoTable(subTest, testConn);
-            Reflux.SelectFromTable(typeof(SupObj), testConn, new System.Collections.Generic.Dictionary<string, object> { });
+            SupObj temp = (SupObj)Reflux.SelectFromTable(typeof(SupObj), testConn, new System.Collections.Generic.Dictionary<string, object> { })[0];
+
+            Console.WriteLine(temp.myObj.Name);
 
 
 
